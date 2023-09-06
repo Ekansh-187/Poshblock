ea8e089e19248c4b120946abd50663a05997eecd
Ekansh Panwar <70100392+Ekansh-187@users.noreply.github.com>
2023-09-06 15:43:19 +0530

    Delete client directory

--- client/src/context/context.js
+++
@@ -1,4 +0,0 @@
-import Poshblock from "./Poshblock.json";
-
-export const AgreementAddress = "0xcd33768F8b73051F9e25Fb266aAD085A70896edB";
-export const AgreementAbi = Poshblock.abi;
--- client/src/pages/Admin/fetchAll.js
+++
@@ -1,9 +0,0 @@
-import { useMutation, useQueryClient, useQuery } from "@tanstack/react-query";
-import newRequest from "../../utils/newRequest";
-export default func = useQuery({
-  queryKey: ["getContract"],
-  queryFn: async () =>
-    await newRequest.get(`/contracts/all`).then((res) => {
-      return res.data;
-    }),
-});
